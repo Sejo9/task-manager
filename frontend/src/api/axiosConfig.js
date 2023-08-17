86ebc29948ddb522e5055f478c71331a595cccf5
Jose Michael Moses <46286891+Sejo9@users.noreply.github.com>
2023-08-17 15:33:51 +0000

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL:"http://localhost:8080/api/v1/tasks"
+    baseURL:"task-manager-backend-production.up.railway.app/api/v1/tasks"
 })
